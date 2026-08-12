15faf7ae3ca0f68ee8ced146dd100a3fd2ade219
fukefuke99 <a01093137913@gmail.com>
2026-08-12 12:09:16 +0900

    fix: conf fix2

--- a.c
+++ a.c
@@ -1,12 +1,7 @@
 #include <stdio.h>
 int main(void)
 {
-<<<<<<< HEAD
-    printf("hell 퓨려sdaafoasdjvlsadnvlsdlcmlmnldnㅑ어포o");
-
-=======
     printf("bye");
     printf("world");
->>>>>>> 107f6f4b7200b2ac845556a6f1ed9b422bccfb39
     return 0;
 }
